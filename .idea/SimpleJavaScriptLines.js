962750ac175044814874ac1dc8e52a54ccf936ca
RayaneSantos <rayane.santos@aluno.uece.br>
2019-02-02 17:19:27 -0300

    This is just a initial test.

---
+++ .idea/SimpleJavaScriptLines.js
@@ -0,0 +1,12 @@
+function obtemPacienteDoFormulario(form) {
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
